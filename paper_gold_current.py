e2c8f02d4be45ee5a2ec21e5c2a2b23cf39f7ef2
Lucas <lucasdekam@gmail.com>
2024-01-24 19:15:49 +0100

    only plot fbv (later make figure with marcus)

--- paper_gold_current.py
+++ paper_gold_current.py
@@ -78,14 +78,14 @@ for i, p_h in enumerate(ph_list):
         reorg=REORG,
     )
 
-fig = plt.figure(figsize=(6.69423, 4))
-gs = GridSpec(nrows=2, ncols=3)
+fig = plt.figure(figsize=(6.69423, 2))
+gs = GridSpec(nrows=1, ncols=3)
 ax1 = fig.add_subplot(gs[0, 0])
 ax2 = fig.add_subplot(gs[0, 1])
 ax3 = fig.add_subplot(gs[0, 2])
-ax1_marcus = fig.add_subplot(gs[1, 0])
-ax2_marcus = fig.add_subplot(gs[1, 1])
-ax3_marcus = fig.add_subplot(gs[1, 2])
+# ax1_marcus = fig.add_subplot(gs[1, 0])
+# ax2_marcus = fig.add_subplot(gs[1, 1])
+# ax3_marcus = fig.add_subplot(gs[1, 2])
 
 colors = P.get_color_gradient(len(conc_list))
 red = P.get_color_gradient(len(gamma_list), color="red")
@@ -98,12 +98,12 @@ for i, conc in enumerate(conc_list):
         color=colors[i],
         label=f"{conc*1e3:.0f}",
     )
-    ax1_marcus.plot(
-        potentials + C.AU_PZC_SHE_V + 59e-3 * DEFAULT_P_H_CONCENT,
-        current_conc_marcus[i, :] * 1e-1,
-        color=colors[i],
-        label=f"{conc*1e3:.0f}",
-    )
+    # ax1_marcus.plot(
+    #     potentials + C.AU_PZC_SHE_V + 59e-3 * DEFAULT_P_H_CONCENT,
+    #     current_conc_marcus[i, :] * 1e-1,
+    #     color=colors[i],
+    #     label=f"{conc*1e3:.0f}",
+    # )
 
 for i, gamma in enumerate(gamma_list):
     ax2.plot(
@@ -112,12 +112,12 @@ for i, gamma in enumerate(gamma_list):
         color=red[i],
         label=f"{gamma:.0f}",
     )
-    ax2_marcus.plot(
-        potentials + C.AU_PZC_SHE_V + 59e-3 * DEFAULT_P_H_CATSIZE,
-        current_gamma_marcus[i, :] * 1e-1,
-        color=red[i],
-        label=f"{gamma:.0f}",
-    )
+    # ax2_marcus.plot(
+    #     potentials + C.AU_PZC_SHE_V + 59e-3 * DEFAULT_P_H_CATSIZE,
+    #     current_gamma_marcus[i, :] * 1e-1,
+    #     color=red[i],
+    #     label=f"{gamma:.0f}",
+    # )
 
 for i, p_h in enumerate(ph_list):
     ax3.plot(
@@ -126,12 +126,12 @@ for i, p_h in enumerate(ph_list):
         color=purple[i],
         label=f"{p_h:.0f}",
     )
-    ax3_marcus.plot(
-        potentials + C.AU_PZC_SHE_V + 59e-3 * p_h,
-        current_ph_marcus[i, :] * 1e-1,
-        color=purple[i],
-        label=f"{p_h:.0f}",
-    )
+    # ax3_marcus.plot(
+    #     potentials + C.AU_PZC_SHE_V + 59e-3 * p_h,
+    #     current_ph_marcus[i, :] * 1e-1,
+    #     color=purple[i],
+    #     label=f"{p_h:.0f}",
+    # )
 
 
 ax1.legend(loc="lower right", frameon=False, title=r"$c^*$ / mM")
@@ -139,11 +139,11 @@ ax2.legend(loc="lower right", frameon=False, title=r"$\gamma_+$")
 ax3.legend(loc="lower right", frameon=False, title=r"pH")
 
 ax1.set_ylim([-1.5, 0])
-ax1_marcus.set_ylim([-1.5, 0])
+# ax1_marcus.set_ylim([-1.5, 0])
 ax2.set_ylim([-20, 0])
-ax2_marcus.set_ylim([-20, 0])
+# ax2_marcus.set_ylim([-20, 0])
 ax3.set_ylim([-3, 0])
-ax3_marcus.set_ylim([-3, 0])
+# ax3_marcus.set_ylim([-3, 0])
 
 labels = ["(a)", "(b)", "(c)", "(d)", "(e)", "(f)"]
 for label, axis in zip(labels, fig.axes):
