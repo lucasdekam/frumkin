028b56e21e8eb761217dc6cb67e91bcdc20bd358
Lucas <lucasdekam@gmail.com>
2024-12-25 23:26:43 +0100

    set up LatticeElectrolyte class

---
+++ edl/defaults.py
@@ -0,0 +1,19 @@
+"""
+Constants not included in scipy.constants
+"""
+
+from scipy import constants
+
+WATER_DENSITY = (
+    0.99704702  # water density @25C, g/mL (https://en.wikipedia.org/wiki/Water)
+)
+WATER_MOLAR_MASS = (
+    18.015  # water molar mass, g/mol (https://en.wikipedia.org/wiki/Water)
+)
+WATER_BULK_M = WATER_DENSITY / WATER_MOLAR_MASS * 1e3  # bulk water, mol/L
+WATER_REL_EPS = 78.4  # water bulk permittivity @25C, referenced to eps_0
+# https://en.wikipedia.org/wiki/Relative_permittivity
+WATER_REL_ELEC_EPS = 1.33**2  # water optical/electronic permittivity @20C
+# based on refractive index (https://en.wikipedia.org/wiki/Water)
+DEFAULT_TEMPERATURE = 298  # K
+KAPPA = constants.elementary_charge**2 / constants.epsilon_0 / constants.angstrom
---
+++ edl/species.py
@@ -0,0 +1,171 @@
+"""
+Utility functions for double-layer models
+"""
+
+from typing import List, Union
+from dataclasses import dataclass, field
+from scipy import constants
+import numpy as np
+
+from . import defaults as D
+
+
+def calculate_dipmom(
+    min_eps: float, max_eps: float, temperature: float, concentration: float
+) -> float:
+    """
+    Calculate the effective dipole moment of a solvent molecule based on its measured
+    permittivity, in elementary charge/Angstrom
+    """
+    dipmom = np.sqrt(
+        3
+        * (max_eps - min_eps)
+        * constants.epsilon_0
+        * constants.Boltzmann
+        * temperature
+        / (concentration * 1e3 * constants.Avogadro)
+    )
+    return dipmom / constants.elementary_charge / constants.angstrom
+
+
+@dataclass
+class Ion:
+    """
+    Ion species dataclass
+    """
+
+    name: str
+    size: float  # relative to smallest species, which should have size 1
+    concentration: float  # molar
+    charge: float  # units of elementary charge
+
+    def __str__(self):
+        return f"Ion: {self.name}, Size={self.size}, Concentration={self.concentration} mol/L, Charge={self.charge}e"
+
+    def __repr__(self):
+        return f"Ion(name='{self.name}', size={self.size}, concentration={self.concentration}, charge={self.charge})"
+
+
+@dataclass
+class Solvent:
+    """
+    Solvent species dataclass
+    """
+
+    name: str
+    size: float  # relative to smallest species, which should have size 1
+    concentration: float  # molar
+    dipole_moment: float  # e·A
+
+    def __str__(self):
+        return f"{self.name}: Size={self.size}, Concentration={self.concentration} mol/L, Dipole Moment={self.dipole_moment} e·Å"
+
+    def __repr__(self):
+        return f"Solvent(name='{self.name}', size={self.size}, concentration={self.concentration}, dipole_moment={self.dipole_moment})"
+
+
+@dataclass
+class Water(Solvent):
+    """
+    Default water parameters
+    """
+
+    name: str = "H2O"
+    size: float = 1.0
+    concentration: float = D.WATER_BULK_M
+    dipole_moment: float = field(init=False)
+
+    def __post_init__(self):
+        # Initialize or calculate the dipole moment based on the constants
+        self.dipole_moment = calculate_dipmom(
+            min_eps=D.WATER_REL_ELEC_EPS,
+            max_eps=D.WATER_REL_EPS,
+            temperature=D.DEFAULT_TEMPERATURE,
+            concentration=self.concentration,
+        )
+
+
+Species = Union[Ion, Solvent]
+
+
+class LatticeElectrolyte:
+    """
+    Class for specifying the electrolyte species using the lattice gas description
+    """
+
+    def __init__(self, species: List[Species]):
+        self.species = species
+        self._account_for_decrement()
+
+    def _account_for_decrement(self) -> None:
+        """
+        Update solvent bulk concentrations to account for the reduction in free solvent
+        concentration due to space occupied by (solvated) ions
+        """
+        decrement = 1 - np.sum(self.ion_n0 * self.ion_sizes) / np.sum(
+            self.sol_n0 * self.sol_sizes
+        )
+
+        for spec in self.species:
+            if isinstance(spec, Solvent):
+                spec.concentration = spec.concentration * decrement
+
+    def get_properties(self, species_type: type, property_name: str) -> np.ndarray:
+        """
+        Get properties as numpy array for a certain type of species
+        """
+        return np.array(
+            [
+                getattr(s, property_name)
+                for s in self.species
+                if isinstance(s, species_type)
+            ]
+        )
+
+    @property
+    def ion_concentrations(self):
+        return self.get_properties(Ion, "concentration")
+
+    @property
+    def ion_sizes(self):
+        return self.get_properties(Ion, "size")
+
+    @property
+    def ion_q(self):
+        return self.get_properties(Ion, "charge")
+
+    @property
+    def ion_n0(self):
+        return (
+            self.get_properties(Ion, "concentration")
+            * 1e3
+            * constants.Avogadro
+            * constants.angstrom**3
+        )
+
+    @property
+    def sol_concentrations(self):
+        return self.get_properties(Solvent, "concentration")
+
+    @property
+    def sol_sizes(self):
+        return self.get_properties(Solvent, "size")
+
+    @property
+    def sol_p(self):
+        return self.get_properties(Solvent, "dipole_moment")
+
+    @property
+    def sol_n0(self):
+        return (
+            self.get_properties(Solvent, "concentration")
+            * 1e3
+            * constants.Avogadro
+            * constants.angstrom**3
+        )
+
+    def __str__(self):
+        return "\n".join(str(s) for s in self.species)
+
+    def __repr__(self):
+        return f"Electrolyte(species={self.species})"
