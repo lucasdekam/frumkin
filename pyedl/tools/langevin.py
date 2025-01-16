bbde7e379e8b41bb7dc9f4064929ee14f8dfe828
Lucas <lucasdekam@gmail.com>
2025-01-16 16:00:42 +0100

    use Taylor series & update docs

--- pyedl/tools/langevin.py
+++ pyedl/tools/langevin.py
@@ -8,72 +8,102 @@ import numpy as np
 
 def langevin_x_over_x(x):  # pylint: disable=invalid-name
     """
-    Returns L(x)/x, where L(x) is the Langevin function:
-    L(x) = 1/tanh(x) - 1/x
-    For small x, the function value is 1/3
+    Returns L(x)/x, where L(x) is the Langevin function.
+
+    Parameters
+    ----------
+    x : array_like
+        Input array.
+
+    Returns
+    -------
+    ndarray
+        L(x)/x, where L(x) = 1/tanh(x) - 1/x. For small x, the function value is 1/3.
     """
-    ret = np.zeros(np.atleast_1d(x).shape)
-    select = np.abs(x) > 1e-4
-    ret[select] = (1 / np.tanh(x[select]) - 1 / x[select]) / x[select]
-    ret[~select] = 1 / 3
-    return ret
+    x = np.atleast_1d(x)
+    with np.errstate(divide="ignore", invalid="ignore"):
+        result = (1 / np.tanh(x) - 1 / x) / x
+
+    small_x_mask = np.abs(x) <= 1e-4
+    result[small_x_mask] = 1 / 3 - x[small_x_mask] ** 2 / 45
+
+    return result
 
 
 def langevin_x(x):  # pylint: disable=invalid-name
     """
-    Returns L(x), where L(x) is the Langevin function:
-    L(x) = 1/tanh(x) - 1/x
-    For small x, the function value is zero
+    Returns L(x), where L(x) is the Langevin function.
+
+    Parameters
+    ----------
+    x : array_like
+        Input array.
+
+    Returns
+    -------
+    ndarray
+        L(x), where L(x) = 1/tanh(x) - 1/x. For small x, the function value is zero.
     """
-    ret = np.zeros(np.atleast_1d(x.shape))
-    select = np.abs(x) > 1e-9
-    ret[select] = 1 / np.tanh(x[select]) - 1 / x[select]
-    return ret
+    x = np.atleast_1d(x)
+    with np.errstate(divide="ignore", invalid="ignore"):
+        result = 1 / np.tanh(x) - 1 / x
+
+    small_x_mask = np.abs(x) <= 1e-4
+    result[small_x_mask] = x[small_x_mask] / 3 - x[small_x_mask] ** 3 / 45
+
+    return result
 
 
 def d_langevin_x(x):  # pylint: disable=invalid-name
     """
-    Returns the derivative of the Langevin function to x:
-    dL/dx = 1/x^2 - 1/sinh^2 x
-    For small x, the function value is 1/3
+    Returns the derivative of the Langevin function with respect to x.
+
+    Parameters
+    ----------
+    x : array_like
+        Input array.
+
+    Returns
+    -------
+    ndarray
+        dL/dx = 1/x^2 - 1/sinh^2(x). For small x, the function value is 1/3.
     """
-    ret = np.zeros(np.atleast_1d(x.shape))
+    x = np.atleast_1d(x)
+    with np.errstate(divide="ignore", invalid="ignore", over="ignore"):
+        result = 1 / x**2 - 1 / np.sinh(x) ** 2
 
-    select_small = np.abs(x) < 1e-4
-    select_big = np.abs(x) > 1e2
-    select_normal = ~select_small * ~select_big
+    small_x_mask = np.abs(x) < 1e-4
+    result[small_x_mask] = 1 / 3 - x[small_x_mask] ** 2 / 15
+    large_x_mask = np.isinf(result)
+    result[large_x_mask] = 0
 
-    ret[select_small] = 1 / 3
-    ret[select_normal] = 1 / x[select_normal] ** 2 - 1 / np.sinh(x[select_normal]) ** 2
-    ret[select_big] = 0
-    return ret
+    return result
 
 
-def sinh_x_over_x(x):  # pylint: disable=invalid-name
+def sinh_x_over_x(x):
     """
-    Returns sinh(x)/x. For small x, the function value is one
+    Returns sinh(x)/x.
+
+    Parameters
+    ----------
+    x : array_like
+        Input array.
+
+    Returns
+    -------
+    ndarray
+        sinh(x)/x. For small x, the function value is one. For very large x,
+        returns the maximum float representation.
     """
-    ret = np.zeros(x.shape)
+    x = np.atleast_1d(x)
+    with np.errstate(divide="ignore", invalid="ignore", over="ignore"):
+        result = np.sinh(x) / x
 
-    select_small = np.abs(x) < 1e-9
-    select_big = np.abs(x) > 1e2
-    select_normal = ~select_small * ~select_big
+    small_x_mask = np.abs(x) < 1e-4
+    large_x_mask = np.isinf(result)
 
-    ret[select_small] = 1
-    ret[select_normal] = np.sinh(x[select_normal]) / (x[select_normal])
-    ret[select_big] = 2e41
-    return ret
+    max_float = np.finfo(np.float64).max
+    result[small_x_mask] = 1 + x[small_x_mask] ** 2 / 6
+    result[large_x_mask] = max_float
 
-
-def sinh_x1_over_x1_times_exp_x2(x1, x2):  # pylint: disable=invalid-name
-    """
-    Returns sinh(x1)/x1 * exp(x2).
-    """
-    select = np.abs(x1) < 1e-9
-    ret = np.zeros(x1.shape)
-    exp_diff = np.exp(np.minimum(x1[~select] + x2[~select], 1e2)) - np.exp(
-        np.minimum(-x1[~select] + x2[~select], 1e2)
-    )
-    ret[~select] = exp_diff / 2 / x1[~select]
-    ret[select] = np.exp(x2[select])
-    return ret
+    return result
